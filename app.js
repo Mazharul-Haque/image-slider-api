ed96b13effefd7c9af4c5a9c6d5042064e0725bb
Mazharul-Haque <mshagor009@gmail.com>
2021-02-20 11:58:05 +0600

    toggle add spinner

--- app.js
+++ app.js
@@ -24,11 +24,13 @@ const showImages = (images) => {
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
+    toggleSpinner(false);
   })
 
 }
 
 const getImages = (query) => {
+  toggleSpinner(true);
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
     .then(data => showImages(data.hits))
@@ -75,6 +77,7 @@ const createSlider = () => {
     src="${slide}"
     alt="">`;
     sliderContainer.appendChild(item)
+    
   })
   changeSlide(0)
   timer = setInterval(function () {
@@ -112,6 +115,7 @@ const changeSlide = (index) => {
 //Enter Trigger
 document.getElementById("search")
   .addEventListener("keypress", function (event) {
+
     //event.preventDefault();
     if (event.key == 'Enter') {
       document.getElementById("search-btn").click();
@@ -131,3 +135,16 @@ searchBtn.addEventListener('click', function () {
 sliderBtn.addEventListener('click', function () {
   createSlider()
 })
+//spinner work
+const toggleSpinner = (show) => {
+  const spinner = document.getElementById('loading-spinner');
+  if(show){
+    spinner.classList.remove('d-none');
+  } else{
+    spinner.classList.add('d-none');
+  }
+  
+  
+
+}
+
